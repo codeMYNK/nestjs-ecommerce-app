6e8cc51ea751e8e2ad7befe05bb826fe54734eee
Mayank Kumar <mynk8076@gmail.com>
2026-05-05 01:12:27 +0530

    Auth Authorization Pipes

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Project Description
+  app.setGlobalPrefix('api/v1');
+
+  //Set Global Validation Pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true
+      }
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 8080);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  Logger.error('Error starting server ', error);
+  process.exit(1);
+});
---
+++ api/src/modules/auth/auth.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { AuthResponseDto } from './dto/auth-response.dto';
+import { RefreshTokenGuard } from './guards/refresh-token.guard';
+import { GetUser } from 'src/common/decorators/get-user.decorator';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  //Register API
+  @Post('register')
+  @HttpCode(HttpStatus.OK)
+  @HttpCode(201)
+  async Register(@Body() registerDto: RegisterDto): Promise<AuthResponseDto> {
+    return await this.authService.register(registerDto);
+  }
+
+  //Refresh access token
+  @Post('refresh')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(RefreshTokenGuard)
+  async refresh(@GetUser('id') userId: string): Promise<AuthResponseDto> {
+    return await this.authService.refreshTokens(userId);
+  }
+
+  //Logout user and invalidate refresh token
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtAuthGuard)
+  async logout(@GetUser('id') userId: string): Promise<{ message: string }> {
+    await this.authService.logout(userId);
+    return { message: 'Logged Out Successfully' };
+  }
+
+  //Login
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() loginDto: LoginDto): Promise<AuthResponseDto>{
+    return await this.authService.login(loginDto);
+  }
+}
