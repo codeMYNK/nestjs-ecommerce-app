1d86e755e567a0c0cb914756f196c390a33621fe
Mayank Kumar <mynk8076@gmail.com>
2026-06-28 15:57:25 +0530

    setup pm2 multicore

---
+++ api/ecosystem.config.js
@@ -0,0 +1,26 @@
+const os = require('os');
+
+// 1. Check if Render is forcing a process limit (WEB_CONCURRENCY)
+// 2. If not, fallback to physical CPU cores
+const totalCores = process.env.WEB_CONCURRENCY ? parseInt(process.env.WEB_CONCURRENCY) : os.cpus().length;
+
+console.log("Available Cores (Adjusted): ", totalCores);
+
+module.exports = {
+  apps: [
+    {
+      name: 'ecommerse-nest-backend',
+      script: './dist/src/main.js',
+      
+      // If totalCores evaluates to 1, use 1 instance. Otherwise use 'max'
+      instances: totalCores === 1 ? 1 : 'max', 
+      
+      // If totalCores evaluates to 1, use 'fork'. Otherwise use 'cluster'
+      exec_mode: totalCores === 1 ? 'fork' : 'cluster',
+      
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
